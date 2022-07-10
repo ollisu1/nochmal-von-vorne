38982ff66b408ba61c0ee99ee34dab5f4b4ec00e
C00P3R86 <cooper-jakubke@t-online.de>
2022-07-11 00:29:31 +0200

    new

--- CandO Game/Assets/Scripts/PlayerMovement.cs
+++ CandO Game/Assets/Scripts/PlayerMovement.cs
@@ -13,12 +13,19 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movment;
 
 
+
     // Update is called once per frame
     void Update()
     {
 
         movment.y = Input.GetAxisRaw("Vertical");
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        
     }
 
     void FixedUpdate()
