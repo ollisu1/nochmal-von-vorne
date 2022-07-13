62216df2dd7d18e4a32b32d6bcf93094e6d6755e
ollisu1 <olli-sucker@web.de>
2022-07-13 22:06:01 +0200

    lol

---
+++ CandO Game/Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string textValue;
+    public Text textElement;
+
+    void Start()
+    {
+        textElement.text = textValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textElement.text = textValue;
+    }
+}
